ae7df90123d1a1594944ed1d54c01dee2efe5ffb
choi-seung-min <3235tmdals@gmail.com>
2019-03-23 22:09:45 +0900

    update

--- app/src/main/java/com/example/kakaotalkcopy/Menu1Fragment.java
+++ app/src/main/java/com/example/kakaotalkcopy/Menu1Fragment.java
@@ -21,38 +21,42 @@ public class Menu1Fragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.fragment_menu1, container, false);
-        RecyclerView recyclerView = v.findViewById(R.id.recyclerView);
+
+        recyclerInit(v);
+        getData();
+        return v;
+    }
+
+    private void recyclerInit(View v){
+        RecyclerView recyclerView = v.findViewById(R.id.profileView);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this.getActivity());
         recyclerView.setLayoutManager(linearLayoutManager);
 
         adapter = new RecyclerAdapter();
         recyclerView.setAdapter(adapter);
-
-        getData();
-        return v;
     }
 
     private void getData() {
         // 임의의 데이터입니다.
-        List<String> listTitle = Arrays.asList("국화", "사막", "수국", "해파리", "코알라", "등대", "펭귄", "튤립",
-                "국화", "사막", "수국", "해파리", "코알라", "등대", "펭귄", "튤립");
+        List<String> listTitle = Arrays.asList("김도훈", "소동현", "정재훈", "이희웅", "성기현", "홍성하", "김상은", "김태현",
+                "김영래", "김동휘", "전승민", "이동기", "노시현", "이승우", "박영진", "체어맨");
         List<String> listContent = Arrays.asList(
-                "이 꽃은 국화입니다.",
-                "여기는 사막입니다.",
-                "이 꽃은 수국입니다.",
-                "이 동물은 해파리입니다.",
-                "이 동물은 코알라입니다.",
-                "이것은 등대입니다.",
-                "이 동물은 펭귄입니다.",
-                "이 꽃은 튤립입니다.",
-                "이 꽃은 국화입니다.",
-                "여기는 사막입니다.",
-                "이 꽃은 수국입니다.",
-                "이 동물은 해파리입니다.",
-                "이 동물은 코알라입니다.",
-                "이것은 등대입니다.",
-                "이 동물은 펭귄입니다.",
-                "이 꽃은 튤립입니다."
+                "갓",
+                "PIN&FIT",
+                "WEED",
+                "앙 기분좋아~",
+                "HAHA",
+                "안드로이드",
+                "언디",
+                "분노조절장애입니다",
+                "노빠꾸",
+                "c++ 변태....",
+                "이름 바꿀거임",
+                "동기잇...!",
+                "ㅎㅇㅎㅇ",
+                "ㅎㅇ",
+                "<3",
+                "던진다, 의자!"
         );
         List<Integer> listResId = Arrays.asList(
                 R.drawable.profile,
--- app/src/main/java/com/example/kakaotalkcopy/Menu2Fragment.java
+++ app/src/main/java/com/example/kakaotalkcopy/Menu2Fragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,8 +13,9 @@ public class Menu2Fragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_menu2, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_menu1, container, false);
+        return v;
     }
 
 }
